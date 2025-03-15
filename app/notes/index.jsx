3c1d56a24a1ad4212161402db49693815e01fdc5
Shannqa <shannqa@gmail.com>
2025-03-15 23:15:38 +0100

    Style all notes

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,6 +1,13 @@
 import React, { useContext } from "react";
 import { Link } from "expo-router";
-import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Button,
+  Pressable,
+} from "react-native";
 import { useSQLiteContext } from "expo-sqlite";
 import { AppContext } from "../../helpers/notes_provider";
 
@@ -13,18 +20,26 @@ export default function AllNotes() {
       <FlatList
         data={notes}
         keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        columnWrapperStyle={styles.row}
         renderItem={({ item }) => (
-          <View style={{ padding: 10, borderBottomWidth: 1 }}>
+          <View style={styles.singleNote}>
             <Link
               href={{
                 pathname: "/notes/[id]",
                 params: { id: item.id },
               }}
+              style={styles.box}
+              asChild
             >
-              {item.title}
+              <Pressable>
+                <View style={styles.box}>
+                  <Text style={styles.title}>{item.title}</Text>
+                  <Text>{item.body}</Text>
+                  <Text>{item.id}</Text>
+                </View>
+              </Pressable>
             </Link>
-            <Text>{item.body}</Text>
-            <Text>{item.id}</Text>
           </View>
         )}
       />
@@ -34,5 +49,30 @@ export default function AllNotes() {
 const styles = StyleSheet.create({
   container: {
     padding: 14,
+    flex: 1,
+    width: "100%",
+  },
+  singleNote: {
+    borderWidth: 1,
+    borderColor: "grey",
+    margin: 4,
+    padding: 4,
+    fontSize: 17,
+    flexBasis: 1,
+    flexGrow: 1,
+    flexDirection: "column",
+    flex: 1,
+    height: 150,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  row: {},
+  title: {
+    fontWeight: "bold",
+    fontSize: 17,
+  },
+  box: {
+    width: "100%",
+    height: "100%",
   },
 });
