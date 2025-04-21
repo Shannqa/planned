b307833c0194a43482b32a212520e59e12893172
Shannqa <shannqa@gmail.com>
2025-04-21 08:51:06 +0200

    WIP component to by shared by all index screens

--- helpers/notes_screen.jsx
+++ helpers/notes_screen.jsx
@@ -1,44 +1,205 @@
-function NotesScreen({ data, url }) {
+import React, { useState, useContext, useEffect, useLayoutEffect } from "react";
+import { Link, useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Pressable,
+  useWindowDimensions,
+  LogBox,
+} from "react-native";
+import { NotesContext } from "../../helpers/notes_provider";
+import { lightColors, darkColors, setStyle } from "../../helpers/themes";
+import { SettingsContext } from "../../helpers/settings_provider";
+import { useNavigation } from "@react-navigation/native";
+import RightMenuMulti from "../../helpers/right_menu_multi";
+import LeftMenuMulti from "../../helpers/left_menu_multi";
+import RenderHtml from "react-native-render-html";
+
+function NotesScreen({ screen }) {
+  const { notes, setNotes, openNotes, setOpenNotes, archiveNotes, setArchiveNotes, binNotes, setBinNotes } = useContext(NotesContext);
+  const { currentTheme, setCurrentTheme } = useContext(SettingsContext);
+  let colors = currentTheme == "dark" ? dark : light;
+  // console.log(notes);
+  const [selecting, setSelecting] = useState(false);
+  const [selectedNotes, setSelectedNotes] = useState([]);
+  const router = useRouter();
+  const navigation = useNavigation();
+  const [notesData, setNotesData] = useState([]);
+  const [screenUrl, setScreenUrl] = useState("");
+  
+    // temporary fix for an error with renderHtml
+  (function fixRenderer() {
+    const ignoreErrors = [
+      "Support for defaultProps will be removed",
+      /Support for defaultProps will be removed from function components in a future major release. Use JavaScript default parameters instead./,
+    ];
+    const error = console.error;
+    console.error = (...arg) => {
+      for (const error of ignoreErrors) if (arg[0].includes(error)) return;
+      error(...arg);
+    };
+    LogBox.ignoreLogs(ignoreErrors);
+  })();
+  const [screenTitle, setScreenTitle] = useState("");
+  
+  if (screen == "openIndex") {
+    setScreenTitle("All notes");
+    setNotesData(openNotes);
+    setScreenUrl("notes");
+  } else if (screen == "archiveIndex") {
+    setScreenTitle("Archive");
+    setNotesData(archiveNotes);
+    setScreenUrl("archive");
+  } else if (screen == "binIndex") {
+    setScreenTitle("Bin");
+    setNotesData(binNotes);
+    setScreenUrl("bin")
+  }
+  
+  // set notes for different screens
+  useEffect(() => {
+    if (screen == "openIndex") {
+      const open = notes.filter((note) => note.status == "open");
+      setOpenNotes(open);
+    } else if (screen == "archiveIndex") {
+      const archive = notes.filter((note) => note.status == "archive");
+      setArchiveNotes(archive);
+    } else if (screen == "binIndex") {
+      const bin = notes.filter((note) => note.status == "bin");
+      setBinNotes(open);
+    }
+  }, [notes]);
+
+  useLayoutEffect(() => {
+    const parent = navigation.getParent();
+    parent.setOptions({
+      headerRight: () => (
+        <RightMenuMulti
+          screen={screen}
+          selecting={selecting}
+          startSelecting={startSelecting}
+          stopSelecting={stopSelecting}
+          selectedNotes={selectedNotes}
+        />
+      ),
+    });
+
+    if (selecting) {
+      parent.setOptions({
+        title: "",
+        headerLeft: () => (
+          <LeftMenuMulti
+            screen={screen}
+            selecting={selecting}
+            startSelecting={startSelecting}
+            stopSelecting={stopSelecting}
+            selectedNotes={selectedNotes}
+          />
+        ),
+      });
+    } else {
+      parent.setOptions({
+        title: screenTitle,
+        headerLeft: null,
+      });
+    }
+  }, [navigation, selecting, selectedNotes]);
+
+  function startSelecting() {
+    // console.log(selecting);
+    setSelecting(true);
+    // show slide in menu
+  }
+
+  function stopSelecting() {
+    setSelecting(false);
+    setSelectedNotes([]);
+    // hide slide in menu
+  }
+
+  function toggleSelection(id) {
+    // add or remove note from selection
+
+    if (selectedNotes.includes(id)) {
+      // remove selection
+      const newList = selectedNotes.filter((note_id) => note_id != id);
+      if (new Promise((resolve, reject) => {})) setSelectedNotes(newList);
+      if (newList.length == 0) {
+        setSelecting(false);
+      }
+      console.log("selection", newList);
+    } else {
+      // add note to selection
+      const newList = [...selectedNotes, id];
+      setSelectedNotes(newList);
+      if (!selecting) {
+        setSelecting(true);
+      }
+      // console.log("selection", newList);
+    }
+  }
+  
   return (
-    <View style={setStyle("container", styles, colors)}>
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={2}
-        columnWrapperStyle={styles.row}
-        renderItem={({ item }) => {
-          const isSelected = selectedNotes.includes(item.id);
-          return (
-            <View
-              style={
-                isSelected
-                  ? [styles.singleNote, colors.selected]
-                  : [styles.singleNote, colors.notSelected]
-              }
-            >
-              <Pressable
-                onPress={
-                  selecting
-                    ? () => toggleSelection(item.id)
-                    : () => router.push(`${url}/${item.id}/view`)
+    <>
+      <View style={setStyle("container", styles, colors)}>
+        <FlatList
+          data={notesData}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          columnWrapperStyle={styles.row}
+          renderItem={({ item }) => {
+            const isSelected = selectedNotes.includes(item.id);
+            return (
+              <View
+                style={
+                  isSelected
+                    ? [styles.singleNote, colors.selected]
+                    : [styles.singleNote, colors.notSelected]
                 }
-                onLongPress={() => toggleSelection(item.id)}
-                style={styles.box}
               >
+                <Pressable
+                  onPress={
+                    selecting
+                      ? () => toggleSelection(item.id)
+                      : () => router.push(`${screenUrl}/${item.id}/view`)
+                  }
+                  onLongPress={() => toggleSelection(item.id)}
+                  style={styles.box}
+                >
+                  <View>
+                    <Text style={setStyle("title", styles, colors)}>
+                      {item.title}
+                    </Text>
+                    {/* <Text style={setStyle("text", styles, colors)}>
+                      {item.body}
+                    </Text> */}
+                    <RenderHtml
+                      source={{ html: item.body }}
+                      contentWidth={200}
+                    />
+                  </View>
+                </Pressable>
+              </View>
+            );
+          }}
+        />
+        {screen == "openIndex" && <View style={setStyle("buttonContainer", styles, colors)}>
+          <View style={setStyle("addButton", styles, colors)}>
+            <Link href="notes/new_note/" asChild>
+              <Pressable>
                 <View>
-                  <Text style={setStyle("title", styles, colors)}>
-                    {item.title}
-                  </Text>
-                  <Text style={setStyle("text", styles, colors)}>
-                    {item.body}
+                  <Text style={setStyle("addButtonText", styles, colors)}>
+                    +
                   </Text>
                 </View>
               </Pressable>
-            </View>
-          );
-        }}
-      />
-    </View>
+            </Link>
+          </View>
+        </View>}
+      </View>
+    </>
   );
 }
 
@@ -77,6 +238,20 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "flex-end",
   },
+  addButton: {
+    width: 80,
+    height: 80,
+    position: "absolute",
+    bottom: 32,
+    right: 0,
+    borderRadius: 50,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addButtonText: {
+    fontSize: 48,
+  },
 });
 
 const light = StyleSheet.create({
@@ -99,6 +274,12 @@ const light = StyleSheet.create({
     backgroundColor: lightColors.primary,
     boxShadow: "2 2 2 lightgrey",
   },
+  addButton: {
+    backgroundColor: lightColors.detail,
+  },
+  addButtonText: {
+    color: lightColors.font,
+  },
 });
 
 const dark = StyleSheet.create({
@@ -120,5 +301,11 @@ const dark = StyleSheet.create({
   notSelected: {
     backgroundColor: darkColors.primary,
     boxShadow: "2 2 2 rgba(0, 0, 0, 0.8)",
+  },
+  addButton: {
+    backgroundColor: darkColors.detail,
+  },
+  addButtonText: {
+    color: darkColors.font,
   },
 });
